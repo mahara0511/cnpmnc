7fa9a1668b95b1c6cb94802ed8d1f040c52933ac
Doãn Đình Hảo <doandinhhao2004@gmail.com>
2025-11-01 10:45:39 +0700

    fix models

--- src/main/java/com/example/restservice/entity/User.java
+++ src/main/java/com/example/restservice/entity/User.java
@@ -11,6 +11,7 @@ import java.time.Instant;
 @NoArgsConstructor
 @AllArgsConstructor
 @Inheritance(strategy = InheritanceType.JOINED)
+@Table(name ="users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
