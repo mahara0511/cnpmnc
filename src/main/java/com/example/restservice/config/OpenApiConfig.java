e0463628c4682a084f0970527dc352734ade0e59
mahara0511 <khanhzip14@gmail.com>
2025-11-08 10:09:14 +0700

    Fix: swagger

--- src/main/java/com/example/restservice/config/OpenApiConfig.java
+++ src/main/java/com/example/restservice/config/OpenApiConfig.java
@@ -5,20 +5,25 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import io.swagger.v3.oas.models.servers.Server;
-
 
 @Configuration
 public class OpenApiConfig {
 
     private static final String SECURITY_SCHEME_NAME = "bearerAuth";
 
+    @Value("${swagger.server.url}")
+    private String swaggerServerUrl;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-            .info(new Info().title("REST Service API").version("1.0.0"))
+            .info(new Info()
+                .title("REST Service API")
+                .version("1.0.0"))
             .components(new Components().addSecuritySchemes(
                 SECURITY_SCHEME_NAME,
                 new SecurityScheme()
@@ -26,7 +31,7 @@ public class OpenApiConfig {
                     .scheme("bearer")
                     .bearerFormat("JWT")))
             .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
-            .addServersItem(new Server().url("http://localhost:8080"))
-            .addServersItem(new Server().url("https://be.cnpmnc.khanhzip14.io.vn"));
+            // 👇 Đặt đúng vị trí nối tiếp, không có dấu chấm phẩy trước đó
+            .addServersItem(new Server().url(swaggerServerUrl));
     }
 }
